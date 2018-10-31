77c88c9c6a79909af899debca7fdd3546dcdc423
stonewell <jingnan.si@gmail.com>
2018-10-30 19:09:36 -0700

    discovery

--- tests/discover.py
+++ tests/discover.py
@@ -11,11 +11,11 @@ msg = \
 # Set up UDP socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 s.settimeout(2)
-s.sendto(msg, ('239.255.255.250', 1900) )
+s.sendto(msg.encode(), ('239.255.255.250', 1900) )
 
 try:
     while True:
         data, addr = s.recvfrom(65507)
-        print addr, data
+        print(addr, data)
 except socket.timeout:
     pass
