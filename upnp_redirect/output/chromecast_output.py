a0f95d2d366b680963fd3f68ca1f17ec66be94b1
stonewell <jingnan.si@gmail.com>
2018-11-02 21:24:16 -0700

    add set media uri for chromecast

--- upnp_redirect/output/chromecast_output.py
+++ upnp_redirect/output/chromecast_output.py
@@ -19,3 +19,17 @@ class ChromecastOutput(OutputBase):
         super(ChromecastOutput, self).__init__()
         self._cast = cast
         self._mc = cast.media_controller
+
+    def OnPlay(self, *args, **kwargs):
+        logging.warning('Not implemented, OnPlay: args={}, kwargs={}'.format(args, kwargs))
+        return 0
+
+    def OnStop(self, *args, **kwargs):
+        logging.warning('Not implemented, OnStop: args={}, kwargs={}'.format(args, kwargs))
+        return 0
+
+    def OnSetMediaURI(self, *args, **kwargs):
+        logging.debug('OnSetMediaURI: args={}'.format(args))
+        self._mc.play_media(args[0], 'video/mp4')
+        self._mc.block_until_active()
+        return 0
