fe36defea4d13e97f48751d7beca67de770a7435
stonewell <jingnan.si@gmail.com>
2018-11-03 11:30:43 -0700

    add block call for media controller, and seek command

--- upnp_redirect/output/chromecast_output.py
+++ upnp_redirect/output/chromecast_output.py
@@ -1,6 +1,7 @@
 import pychromecast
 import logging
-
+import time
+import datetime
 from . import OutputBase
 
 
@@ -18,23 +19,40 @@ class ChromecastOutput(OutputBase):
     def __init__(self, cast):
         super(ChromecastOutput, self).__init__()
         self._cast = cast
-        self._mc = cast.media_controller
 
     def OnPlay(self, *args, **kwargs):
         logging.debug('OnPlay')
-        self._mc.play()
+        self._cast.media_controller.play()
+        self._cast.media_controller.block_until_active()
 
     def OnPause(self, *args, **kwargs):
         logging.debug('OnPause')
-        self._mc.pause()
+        self._cast.media_controller.pause()
+        self._cast.media_controller.block_until_active()
 
     def OnStop(self, *args, **kwargs):
         logging.debug('OnStop')
-        self._mc.stop()
+        self._cast.media_controller.stop()
+        self._cast.media_controller.block_until_active()
         return 0
 
     def OnSetMediaURI(self, *args, **kwargs):
         logging.debug('OnSetMediaURI: args={}'.format(args))
-        self._mc.play_media(args[0], 'video/mp4')
-        self._mc.block_until_active()
+        self._cast.play_media(args[0], 'video/mp4')
+        self._cast.media_controller.block_until_active()
+        return 0
+
+    def OnSeek(self, *args, **kwargs):
+        t, unit = args
+
+        if unit == 'REL_TIME':
+            st = time.strptime(t, '%H:%M:%S')
+            interval = datetime.timedelta(seconds=st.tm_sec,
+                                          hours=st.tm_hour,
+                                          minutes=st.tm_min)
+            self._cast.media_controller.seek(interval.total_seconds())
+            self._cast.media_controller.block_until_active()
+            return 0
+
+        logging.warning('Not implemented seek:{}'.format(args))
         return 0
